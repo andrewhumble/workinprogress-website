919d76bdadcd7508dccb67859fdb263bb1d5e123
Andrew Humble <andrewhumble@me.com>
2023-02-22 21:51:47 -0500

    Commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,9 +14,9 @@ class MyDocument extends Document {
           />
           <link rel="icon" href="/favicon.ico" />
           <link
-            rel="apple-touch-icon"
+            rel="wip-logo"
             sizes="192x192"
-            href="/apple-touch-icon.png"
+            href="/wip_logo.png"
           ></link>
           <link
             rel="alternate"
